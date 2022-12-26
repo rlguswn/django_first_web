8ef601295f89bc19b11dc588daa08597afcd5476
ptustudent <0318joo@naver.com>
2022-12-26 11:52:58 +0900

    Create blog post detail page(class based)

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     # 페이지 번호 자동 지정
-    path('<int:pk>/', views.single_post_page),
-    path('', views.index),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view())
+    # path('', views.index),
 ]
