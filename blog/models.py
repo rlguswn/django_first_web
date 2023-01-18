cb487ce91363bc0db52db7f932f0199a2d5e271b
ptustudent <0318joo@naver.com>
2023-01-18 17:34:31 +0900

    Update landing page

--- blog/models.py
+++ blog/models.py
@@ -61,6 +61,12 @@ class Post(models.Model):
     def get_content_markdown(self):
         return markdown(self.content)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/1421/2d73a62a74a7773d/svg/{self.author.email}'
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
--- single_pages/tests.py
+++ single_pages/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from bs4 import BeautifulSoup
+from blog.models import Post
 
-# Create your tests here.
+
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user_rlguswn = User.objects.create_user(username='rlguswn', password='somepassword')
+
+    def test_landing(self):
+        post_001 = Post.objects.create(
+            title='첫 번째 포스트입니다.',
+            content='Hello World',
+            author=self.user_rlguswn
+        )
+
+        post_002 = Post.objects.create(
+            title='두 번째 포스트입니다.',
+            content='We are same',
+            author=self.user_rlguswn
+        )
+
+        post_003 = Post.objects.create(
+            title='세 번째 포스트입니다.',
+            content='No category',
+            author=self.user_rlguswn
+        )
+
+        post_004 = Post.objects.create(
+            title='네 번째 포스트입니다.',
+            content='This is not a post',
+            author=self.user_rlguswn
+        )
+
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        body = soup.body
+        self.assertNotIn(post_001.title, body.text)
+        self.assertIn(post_002.title, body.text)
+        self.assertIn(post_003.title, body.text)
+        self.assertIn(post_004.title, body.text)
+
